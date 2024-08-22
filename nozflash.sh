5de961f3979516c58e55d0bdb7696b9e39d76d12
StaticMdz <45854404+StaticM223@users.noreply.github.com>
2024-08-22 00:40:27 +0000

    Update nozflash.sh

--- nozflash.sh
+++ nozflash.sh
@@ -1,13 +1,40 @@
+FILE1="noz0_120_G30-noz0_007_000.bin"
+FILE2="noz0_120_G30-noz0_009_000.bin"  # Alternative file to try if FILE1 is not found
+URL1="https://github.com/StaticM223/K1MAXM5P/raw/main/noz0_120_G30-noz0_014_000.bin"
+
 # Check for /usr/share/klipper directory
 if [ -d /usr/share/klipper ]; then
-    rm /usr/share/klipper/fw/K1/noz0_120_G30-noz0_014_000.bin
-    wget --no-check-certificate -P /usr/share/klipper/fw/K1/ https://github.com/StaticM223/K1MAXM5P/raw/main/noz0_120_G30-noz0_014_000.bin
+    if [ -f /usr/share/klipper/fw/K1/$FILE1 ]; then
+        rm /usr/share/klipper/fw/K1/$FILE1
+    elif [ -f /usr/share/klipper/fw/K1/$FILE2 ]; then
+        rm /usr/share/klipper/fw/K1/$FILE2
+    fi
+    wget --no-check-certificate -P /usr/share/klipper/fw/K1/ $URL1
 
 # Check for /usr/data/klipper directory
 elif [ -d /usr/data/klipper ]; then
-    rm /usr/data/klipper/fw/K1/noz0_120_G30-noz0_014_000.bin
-    wget --no-check-certificate -P /usr/data/klipper/fw/K1/ https://github.com/StaticM223/K1MAXM5P/raw/main/noz0_120_G30-noz0_014_000.bin
+    if [ -f /usr/data/klipper/fw/K1/$FILE1 ]; then
+        rm /usr/data/klipper/fw/K1/$FILE1
+    elif [ -f /usr/data/klipper/fw/K1/$FILE2 ]; then
+        rm /usr/data/klipper/fw/K1/$FILE2
+    fi
+    wget --no-check-certificate -P /usr/data/klipper/fw/K1/ $URL1
 
 else
-    echo "Neither /usr/share/klipper nor /usr/data/klipper directory exists."
+    echo "Klipper isnt installed! you must install it first or reset your printer and restart the install from step #1"
 fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

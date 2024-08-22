47451a2e6a52b68eb5b5199a09166a0705b0104b
StaticMdz <45854404+StaticM223@users.noreply.github.com>
2024-08-22 00:14:12 +0000

    Update nozflash.sh

--- nozflash.sh
+++ nozflash.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Check for /usr/share/klipper directory
-if [ -d /klipper ]; then
+if [ -d /home/biqu/klipper ]; then
     rm /klipper/fw/K1/noz0_120_G30-noz0_003_000.bin
-    wget --no-check-certificate -P /klipper/fw/K1/ https://github.com/StaticM223/K1MAXM5P/raw/main/noz0_120_G30-noz0_014_000.bin
+    wget --no-check-certificate -P /home/biqu/klipper/fw/K1/ https://github.com/StaticM223/K1MAXM5P/raw/main/noz0_120_G30-noz0_014_000.bin
 
 # Check for /usr/data/klipper directory
-elif [ -d /klipper ]; then
+elif [ -d /home/biqu/klipper ]; then
     rm /klipper/fw/K1/noz0_120_G30-noz0_003_000.bin
-    wget --no-check-certificate -P /klipper/fw/K1/ https://github.com/StaticM223/K1MAXM5P/raw/main/noz0_120_G30-noz0_014_000.bin
+    wget --no-check-certificate -P /home/biqu/klipper/fw/K1/ https://github.com/StaticM223/K1MAXM5P/raw/main/noz0_120_G30-noz0_014_000.bin
 
 else
     echo "Klipper directory does NOT exist."

a473f1c46c5d21dbc426195f304a580847bcafd8
StaticMdz <45854404+StaticM223@users.noreply.github.com>
2024-08-22 00:30:21 +0000

    Update nozflash.sh

--- nozflash.sh
+++ nozflash.sh
@@ -1,11 +1,11 @@
 # Check for /usr/share/klipper directory
 if [ -d /usr/share/klipper ]; then
-    rm /usr/share/klipper/fw/K1/noz0_120_G30-noz0_003_000.bin
+    rm /usr/share/klipper/fw/K1/noz0_120_G30-noz0_014_000.bin
     wget --no-check-certificate -P /usr/share/klipper/fw/K1/ https://github.com/StaticM223/K1MAXM5P/raw/main/noz0_120_G30-noz0_014_000.bin
 
 # Check for /usr/data/klipper directory
 elif [ -d /usr/data/klipper ]; then
-    rm /usr/data/klipper/fw/K1/noz0_120_G30-noz0_003_000.bin
+    rm /usr/data/klipper/fw/K1/noz0_120_G30-noz0_014_000.bin
     wget --no-check-certificate -P /usr/data/klipper/fw/K1/ https://github.com/StaticM223/K1MAXM5P/raw/main/noz0_120_G30-noz0_014_000.bin
 
 else
